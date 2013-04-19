7964f69196f02b494d8117042beb431e923c543b
uzzu <himeatball@gmail.com>
2013-04-19 14:07:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Usage
+    # sudo chmod u+x *
+    # sudo ./change_keyboard_(jp|us).sh
---
+++ change_keyboard_us.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo $0: Running.
+cp /usr/local/src/keyboard/keyboard_us /etc/sysconfig/keyboard
+echo $0: Complete. Please reboot or logout.
